49f5880531a25a234f075f79191f6c8ada11de04
Aerelloth <oceane.scheidel@gmail.com>
2017-04-10 22:34:51 +0200

    Ajout du fichier "configuration" sans les infos confidentielles

---
+++ application/configuration.php
@@ -0,0 +1,16 @@
+<?php 	
+
+define('DATABASE_DSN', 'mysql:host=*****;dbname=*****;charset=utf8');
+define('DATABASE_USER_NAME', '*****');
+define('DATABASE_PASSWORD', '*****');
+define('DEFAULT_CONTROLLER_NAME', 'products');
+define('DEFAULT_ACTION_NAME', 'showSome');
+define('DEFAULT_TEMPLATE_PATH', 'default.phtml');
+define('PRODUCTS_BY_PAGE', 10);
+define('ORDERS_BY_PAGE', 1);
+define('ADMIN_EMAIL', '*****');
+define('ADMIN_CUSTOMERS_BY_PAGE', 5);
+define('ADMIN_CATEGORIES_BY_PAGE', 5);
+define('ADMIN_ORDERS_BY_PAGE', 1);
+define('ADMIN_PRODUCTS_BY_PAGE', 10);
+ ?>
